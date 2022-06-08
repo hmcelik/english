b840c27e06b2f1591c6e6be4bf38b7bf1b31c3fd
huseyinmelihcelik <huseyinmelihcelik@gmail.com>
2022-06-08 08:55:10 +0300

    i

---
+++ javascript/theme.js
@@ -0,0 +1,24 @@
+lines (18 sloc)  525 Bytes
+   
+const themeMap = {
+  dark: "light",
+  light: "solar",
+  solar: "dark"
+};
+
+const theme = localStorage.getItem('theme')
+  || (tmp = Object.keys(themeMap)[0],
+      localStorage.setItem('theme', tmp),
+      tmp);
+const bodyClass = document.body.classList;
+bodyClass.add(theme);
+
+function toggleTheme() {
+  const current = localStorage.getItem('theme');
+  const next = themeMap[current];
+
+  bodyClass.replace(current, next);
+  localStorage.setItem('theme', next);
+}
+
+document.getElementById('themeButton').onclick = toggleTheme;
